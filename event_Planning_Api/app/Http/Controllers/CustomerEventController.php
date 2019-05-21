40d9fbb21ee7eeb958a59e1369cf7a14d661f54c
arishali56 <umer.ali.16.ua@gmail.com>
2019-05-21 23:01:37 +0500

    minor fix

--- event_Planning_Api/app/Http/Controllers/CustomerEventController.php
+++ event_Planning_Api/app/Http/Controllers/CustomerEventController.php
@@ -210,13 +210,13 @@ public function get_order_approved(Request $request,$type){
         $customer=DB::select("select customer_id from customers where username = '$user->name'");
     
         if($type=="services"){
-        $services=services::select('o_id','order_status','payment_method','payment_status','description','order_type',
+        $services=orders::select('o_id','order_status','payment_method','payment_status','description','order_type',
         'service_id','customer_id','event_id')->where('order_status','pending')->where('order_type','service')
         ->where('customer_id',$customer[0]->customer_id)->paginate(15);
         return $services;
     }
     else if($type=="packages"){
-        $services=services::select('o_id','order_status','payment_method','payment_status','description','order_type',
+        $services=orders::select('o_id','order_status','payment_method','payment_status','description','order_type',
         'package_id','customer_id','event_id')->where('order_status','pending')->where('order_type','package')
         ->where('customer_id',$customer[0]->customer_id)->paginate(15);
         return $services;
@@ -224,6 +224,12 @@ public function get_order_approved(Request $request,$type){
 }
 }
 
+public function get_expenses($id){
+    if($user->user_type=="customer"){
+        $customer=DB::select("select customer_id from customers where username = '$user->name'");
+        $events= 
+    }
+}
 
 public function update_payment_status (Request $request,$id){
     $user=User::findOrFail(Auth::guard('api')->id());
